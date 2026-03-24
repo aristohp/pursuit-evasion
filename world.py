f83e78bb3512ebcfa3c1ab067fdb10d41b1c034f
Aristo <aristopanirman4@gmail.com>
2026-03-24 00:48:10 -0400

    add projects comments

---
+++ world.py
@@ -0,0 +1,7 @@
+# Pursuit-Evasion Simulator
+# Started: March 2026
+#
+# Goal: model the cops & robbers problem on a 2D grid
+# Agent 1: Pursuer
+# Agent 2: Evader
+# Environment: grid with obstacles
